a78c2cdba142aeb23b36345967880c1795ea22b7
Yannick Comte <yannick.comte@novelab.net>
2024-06-05 10:34:33 +0200

    Initial commit

---
+++ Unreal Launcher/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Unreal Launcher
+//
+//  Created by Yannick Comte on 04/06/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Unreal Launcher/Unreal_LauncherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Unreal_LauncherApp.swift
+//  Unreal Launcher
+//
+//  Created by Yannick Comte on 04/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct Unreal_LauncherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
